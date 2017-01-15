cf2f71912e738cc6b3dbaf3e22246acf375655f7
null00025 <null00025@gmail.com>
2017-01-15 23:53:14 +0900

    パラメータ判定処理いれてみた。

--- test001/src/main/java/test001/HelloWorld.java
+++ test001/src/main/java/test001/HelloWorld.java
@@ -2,6 +2,9 @@ package test001;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("パラメータあるよ。");
+		}
 		System.out.println("Hello World");
 	}
 
