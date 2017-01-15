97cc62d1804f29d64c8e6ec062ec0832378f57a7
null00025 <null00025@gmail.com>
2017-01-15 23:58:56 +0900

    戻しました。

--- test001/src/main/java/test001/HelloWorld.java
+++ test001/src/main/java/test001/HelloWorld.java
@@ -2,10 +2,6 @@ package test001;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			System.out.println("パラメータあるよ。");
-		}
 		System.out.println("Hello World");
 	}
-
 }
